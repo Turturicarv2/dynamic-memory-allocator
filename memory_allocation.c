a490a44bb498a88573534654ea99ae13872d928b
turturicarv2 <daniel.plamadeala03@gmail.com>
2024-11-25 08:42:04 +0200

    add zero allocation test

--- memory_allocation.c
+++ memory_allocation.c
@@ -5,6 +5,7 @@
 memory_chunk_t *first_memory_chunk = NULL;
 
 
+/* main allocation function that returns pointer to free memory address */
 void *allocate_memory(__uint16_t needed_memory_size)
 {
     /* if no memory is needed, then exit function */
@@ -29,7 +30,7 @@ void *allocate_memory(__uint16_t needed_memory_size)
     memory_chunk_t *current_chunk = first_memory_chunk;
     while(current_chunk != NULL)
     {
-        if((current_chunk->metadata.in_use == true)
+        if((current_chunk->metadata.in_use == IN_USE)
                 || (current_chunk->metadata.chunk_size < needed_memory_size))
         {
             current_chunk = current_chunk->next_chunk;
@@ -39,7 +40,7 @@ void *allocate_memory(__uint16_t needed_memory_size)
         else if((current_chunk->metadata.chunk_size == needed_memory_size)
                 || (current_chunk->metadata.chunk_size - CHUNK_STRUCT_SIZE < needed_memory_size))
         {
-            current_chunk->metadata.in_use = true;
+            current_chunk->metadata.in_use = IN_USE;
             return (void *)((__uint8_t *)current_chunk + CHUNK_STRUCT_SIZE);
         }
         else
@@ -54,6 +55,7 @@ void *allocate_memory(__uint16_t needed_memory_size)
 }
 
 
+/* function that requests memory from the heap */
 static void initialize_dynamic_memory()
 {
     /* Allocate a large block of memory using mmap */
@@ -71,11 +73,12 @@ static void initialize_dynamic_memory()
     /* Initialize first memory chunk */
     first_memory_chunk = (memory_chunk_t *)memory_address;
     first_memory_chunk->metadata.chunk_size = 4096 - CHUNK_STRUCT_SIZE;
-    first_memory_chunk->metadata.in_use = false;
+    first_memory_chunk->metadata.in_use = NOT_IN_USE;
     first_memory_chunk->next_chunk = NULL;
 }
 
 
+/* function that splits a bigger chunk into 2 smaller ones */
 static void *chunk_split(memory_chunk_t *initial_chunk, __uint16_t split_size)
 {
     /* create new memory chunk that will hold the remaining size from the initial chunk */
@@ -83,17 +86,18 @@ static void *chunk_split(memory_chunk_t *initial_chunk, __uint16_t split_size)
             ((char *)initial_chunk + split_size + CHUNK_STRUCT_SIZE);
     new_memory_chunk->metadata.chunk_size = 
             (initial_chunk->metadata.chunk_size - split_size - CHUNK_STRUCT_SIZE);
-    new_memory_chunk->metadata.in_use = false;
+    new_memory_chunk->metadata.in_use = NOT_IN_USE;
     new_memory_chunk->next_chunk = initial_chunk->next_chunk;
 
     /* initial chunk will shrink to fit the size needed and get returned */
     initial_chunk->metadata.chunk_size = split_size;
-    initial_chunk->metadata.in_use = true;
+    initial_chunk->metadata.in_use = IN_USE;
     initial_chunk->next_chunk = new_memory_chunk;
     return (void *)((char *)initial_chunk + CHUNK_STRUCT_SIZE);
 }
 
 
+/* function that frees a chunk of memory */
 void free_memory(void *chunk)
 {
     /* search through the linked list to see if 
@@ -119,18 +123,18 @@ void free_memory(void *chunk)
             0, 
             current_chunk->metadata.chunk_size - CHUNK_STRUCT_SIZE
     );
-    current_chunk->metadata.in_use = false;
+    current_chunk->metadata.in_use = NOT_IN_USE;
     
     /* memory coalescing */
     memory_chunk_t *next_memory_chunk = current_chunk->next_chunk;
-    if((next_memory_chunk != NULL) && (next_memory_chunk->metadata.in_use == false))
+    if((next_memory_chunk != NULL) && (next_memory_chunk->metadata.in_use == NOT_IN_USE))
     {
         current_chunk->metadata.chunk_size = current_chunk->metadata.chunk_size 
                 + next_memory_chunk->metadata.chunk_size + CHUNK_STRUCT_SIZE;
         current_chunk->next_chunk = next_memory_chunk->next_chunk;
     }
 
-    if((prev_memory_chunk != NULL) && (prev_memory_chunk->metadata.in_use == false))
+    if((prev_memory_chunk != NULL) && (prev_memory_chunk->metadata.in_use == NOT_IN_USE))
     {
         prev_memory_chunk->metadata.chunk_size = current_chunk->metadata.chunk_size
                 + prev_memory_chunk->metadata.chunk_size + CHUNK_STRUCT_SIZE;
--- tests.c
+++ tests.c
@@ -1,17 +1,36 @@
 #include "tests.h"
 
+/* TESTS */
+/* simple allocation test that verifies 
+writing into the requested block of memory */
 Test(memory_allocation, test_simple_allocation)
 {
-    void* block = allocate_memory(16);
-    cr_assert(ne(block, NULL));
+    uint8_t size = 16;
+    void* block = allocate_memory(size);
+    cr_assert(
+        ne(block, NULL), 
+        "assert that a block of memory was allocated successfully"
+    );
 
-    memset(block, 0xA, 16);
+    /* write into the block of memory */
+    memset(block, 0xA, size);
 
     __uint8_t index;
-    for(index = 0; index < 16; index++)
+    for(index = 0; index < size; index++)
     {
-        cr_assert(eq(i32, ((unsigned char*)block)[index], 0xA));
+        cr_assert(
+            eq(i32, ((unsigned char*)block)[index], 0xA), 
+            "assert that writing into the memory block was done successfully"
+        );
     }
 
+    /* free the used block of memory */
     free_memory(block);
 }
+
+Test(memory_allocation, test_zero_allocation)
+{
+    void* block = allocate_memory(0);
+    cr_assert(eq(block, NULL), "assert that a block of memory was not allocated");
+}
+
